a78056273aafd67e9ae89e26dd728e2691f6ade6
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-12-14 16:36:26 +0900

    Setup lint and fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
-<script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+<script lang='ts'>
+  import favicon from '$lib/assets/favicon.svg'
 
-	let { children } = $props();
+  const { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel='icon' href={favicon} />
 </svelte:head>
 
 {@render children()}
